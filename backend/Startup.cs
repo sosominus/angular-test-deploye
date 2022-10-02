e6071fdf004dea1cd770a3f580d7c789c7be6215
sosominus <70543131+sosominus@users.noreply.github.com>
2022-10-02 23:06:35 +0200

    Update Startup.cs

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -98,7 +98,7 @@ namespace prid_tuto
             app.UseSpa(spa => {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
-                spa.Options.SourcePath = "frontend";
+                spa.Options.SourcePath = "frontend/dist";
                 if (env.IsDevelopment())
                 {
                     //spa.UseAngularCliServer(npmScript: "start");
